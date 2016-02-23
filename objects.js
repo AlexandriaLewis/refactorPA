50353191d3e7c0e5921c2a303d0b67bbea41eebf
lexria <alexandriamlewis@me.com>
2016-02-23 14:54:55 -0500

    got a template working

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var photos = {
+  cover: ["photos/architecture01.jpeg", "photos/forest01.jpeg", "photos/night01.jpeg", "photos/path01.jpeg", "photos/wall01.jpeg", "photos/wide01.jpeg"],
   architecture: ["photos/architecture01.jpeg", "photos/architecture02.jpeg", "photos/architecture03.jpeg", "photos/architecture04.jpeg", "photos/architecture05.jpeg", "photos/architecture06.jpeg"],
   forest: ["photos/forest01.jpeg", "photos/forest02.jpeg", "photos/forest03.jpeg", "photos/forest04.jpeg", "photos/forest05.jpeg", "photos/forest06.jpeg"],
   night: ["photos/night01.jpeg", "photos/night02.jpeg", "photos/night03.jpeg", "photos/night04.jpeg", "photos/night05.jpeg", "photos/night06.jpeg"],
@@ -7,11 +8,37 @@ var photos = {
   wide: ["photos/wide01.jpeg", "photos/wide02.jpeg", "photos/wide03.jpeg", "photos/wide04.jpeg", "photos/wide05.jpeg", "photos/wide06.jpeg"]
 };
 
-var coverPhotos = {
-  architecture: "photos/architecture01.jpeg",
-  forest: "photos/forest01.jpeg",
-  night: "photos/night01.jpeg",
-  path: "photos/path01.jpeg",
-  wall: "photos/wall01.jpeg",
-  wide: "photos/wide01.jpeg"
-};
+// --------------------------------------------------------->
+
+var altPhotos = [
+  {
+    title: "architecture",
+    cover: "photos/architecture01.jpeg",
+    photos: ["photos/architecture01.jpeg", "photos/architecture02.jpeg", "photos/architecture03.jpeg", "photos/architecture04.jpeg", "photos/architecture05.jpeg", "photos/architecture06.jpeg"]
+  },
+  {
+    title: "forest",
+    cover: "photos/forest01.jpeg",
+    photos: ["photos/forest01.jpeg", "photos/forest02.jpeg", "photos/forest03.jpeg", "photos/forest04.jpeg", "photos/forest05.jpeg", "photos/forest06.jpeg"]
+  },
+  {
+    title: "night",
+    cover: "photos/night01.jpeg",
+    photos: ["photos/night01.jpeg", "photos/night02.jpeg", "photos/night03.jpeg", "photos/night04.jpeg", "photos/night05.jpeg", "photos/night06.jpeg"]
+  },
+  {
+    title: "path",
+    cover: "photos/path01.jpeg",
+    photos: ["photos/path01.jpeg", "photos/path02.jpeg", "photos/path03.jpeg", "photos/path04.jpeg", "photos/path05.jpeg", "photos/path06.jpeg"]
+  },
+  {
+    title: "wall",
+    cover: "photos/wall01.jpeg",
+    photos: ["photos/wall01.jpeg", "photos/wall02.jpeg", "photos/wall03.jpeg", "photos/wall04.jpeg", "photos/wall05.jpeg", "photos/wall06.jpeg"]
+  },
+  {
+    title: "wide",
+    cover: "photos/wide01.jpeg",
+    photos: ["photos/wide01.jpeg", "photos/wide02.jpeg", "photos/wide03.jpeg", "photos/wide04.jpeg", "photos/wide05.jpeg", "photos/wide06.jpeg"]
+  }
+];
--- templates.js
+++ templates.js
@@ -1,5 +1,56 @@
-var pageOne = _.template($("#home01").html());
+$(document).ready(function(){
+// --------------------------------------------------------->
 
-$('.home01').html(coverPhotos.forEach(function(el){
-  pageOne(el)
-}));
+// --------------------------------------------------------->
+  // var pageOne = _.template($("#home01").html());
+  //
+  // var pageTwo = _.template($("#home02").html());
+
+
+  var homeOne = {};
+
+  homeOne.pageOne = [
+    '<a href="#" rel="albumPg">',
+      '<div class="cookie">',
+        '<div class="cutter">',
+        '<img src = "<%= poopy %>" />',
+        '</div>',
+      '</div>',
+    '</a>'
+  ].join("");
+
+
+var homeOneAlbumHtml = "";
+var homeOneAlbumTmpl = _.template(homeOne.pageOne);
+
+photos.cover.forEach(function (el) {
+  homeOneAlbumHtml += homeOneAlbumTmpl({poopy: el});
+});
+console.log(homeOneAlbumHtml);
+$('.home01').html(homeOneAlbumHtml);
+
+
+  var homeTwo = {};
+
+  homeTwo.pageTwo = [
+    '<a href="#" class="enhance" rel="photoView">',
+      '<div class="cookie">',
+      '<div class="cutter">',
+      '<img src = "<%= photos %>" />',
+      '</div>',
+      '</div>',
+    '</a>'
+  ].join("");
+
+var homeTwoAlbum = "";
+var homeTwoTmpl = _.template(homeTwo.pageTwo);
+
+  altPhotos.forEach(function (el) {
+    el.photos.forEach(function (item) {
+      homeTwoAlbum += homeTwoTmpl({photos: item})
+    });
+    // console.log(homeTwoAlbum);
+  });
+
+// --------------------------------------------------------->
+});
